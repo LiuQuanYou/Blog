fe67995dec0530a8f87b6446e757ace394e68709
杨晓楠 <xnan.yang@flowportal.com>
2023-10-31 17:22:30 +0800

    删除后台管理，拆分前后端。

--- serve/api/openaiRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-// 从controller中引入
-const { generateImage } = require('../controllers/openaiController');
-const router = express.Router();
-
-router.post('/generateimage', generateImage);
-
-module.exports = router;
